cbb6f86bec51ea367bdd90446eefdf6939e3ae24
jkort <jortealv@myuax.com>
2023-03-05 13:13:30 +0100

    NotaMediaVersion4

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -1,2 +1,13 @@
-package org.example;public class Ejercicio1 {
+package org.example;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int[] numeros = new int[11];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = 10 - i;
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(" " + numeros[i]);
+        }
+    }
 }
--- src/main/java/org/example/Ejercicio3.java
+++ src/main/java/org/example/Ejercicio3.java
@@ -1,2 +1,41 @@
-package org.example;public class Ejercicio3 {
+package org.example;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio3{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Double> notas = new ArrayList<>();
+        double suma = 0;
+        int numNotas = 0;
+
+        System.out.println("Introduzca la nota (-1 para terminar):");
+
+        double nota = sc.nextDouble();
+
+        while (nota != -1) {
+
+            notas.add(nota);
+            suma += nota;
+            numNotas++;
+
+            System.out.println("Introduzca la nota (-1 para terminar):");
+
+            nota = sc.nextDouble();
+        }
+
+        double media = numNotas == 0 ? 0 : suma / numNotas;
+
+        System.out.print("Las notas introducidas son: ");
+        for (Double n : notas) {
+            System.out.print(n + "; ");
+        }
+
+        System.out.println("\nLa media de las notas es " + media + ".");
+    }
 }
+
