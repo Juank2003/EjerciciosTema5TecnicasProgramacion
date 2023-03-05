7fc2064be3627425f99094770982e2d6a0a7d2cd
jkort <jortealv@myuax.com>
2023-03-05 13:44:52 +0100

    Ejercicio6

---
+++ src/main/java/org/example/Ejercicio6.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Ejercicio6 {
+/*El algoritmo anterior es una tabla bidimensional de 3x3 con valores del 1 al 9.
+Cada valor se asigna a una posición en la tabla usando dos bucles donde se recorre cada fila y columna de la tabla.*/
+
+    /*1.
+
+    Algo QueHace
+
+    #??
+    Constante TAMAÑO : entero <- 4
+    Variable i, j, val: entero
+    Variable tab2d: entero[TAMAÑO][TAMAÑO]
+    Inicio
+    val <- 1
+    Para i <- 0 a TAMAÑO - 1
+    Para j <- 0 a TAMAÑO - 1
+    tab2d[j][i] <- val
+    val <- val + 1
+    FPara
+    FPara
+    Fin*/
+
+    /*2
+    Algo QueHace
+
+    #??
+    Constante TAMAÑO : entero <- 4
+    Variable i, j: entero
+    Variable tab2d: entero[TAMAÑO][TAMAÑO]
+    Inicio
+    Para j <- 0 a TAMAÑO - 1
+    Para i <- 0 a TAMAÑO - 1
+    tab2d[i][j] <- i + j
+    FPara
+    FPara
+    Fin */
+
+
+}
