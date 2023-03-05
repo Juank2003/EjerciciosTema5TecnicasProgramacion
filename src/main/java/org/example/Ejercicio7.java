98cd79dfcb80e7629c43fee1fdaab19a8a22ec65
jkort <jortealv@myuax.com>
2023-03-05 13:47:43 +0100

    Ejercicio7

--- src/main/java/org/example/Ejercicio7.java
+++ src/main/java/org/example/Ejercicio7.java
@@ -1,4 +1,22 @@
 package org.example;
+import java.util.Random;
 
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        char[][] tabla = new char[20][30];
+        Random random = new Random();
+
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                tabla[i][j] = (char) (random.nextInt(26) + 'a');
+            }
+        }
+
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                System.out.print(tabla[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
